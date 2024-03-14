acf1494ca57360d85cb7c2b635a65fd0b3b9792e
Preet Suthar <preetsutharxd@gmail.com>
2024-03-14 17:20:04 +0530

    Add Hero component and update 3D model rotation

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>GRAB YOURS NOW</h1>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
-import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
 import { OrbitControls, Environment } from "@react-three/drei";
+import Hero from "@/components/Hero";
 
 function Model() {
   const gltf = useLoader(GLTFLoader, "/soda_can.glb");
+  const mesh = useRef();
   gltf.scene.scale.set(0.4, 0.4, 0.4);
-  gltf.scene.rotateZ(1);
-  return <primitive style={{ transform: "scale(0.8)" }} object={gltf.scene} />;
+  gltf.scene.castShadow = true;
+
+  useFrame(({ clock }) => {
+    mesh.current.rotation.y = clock.getElapsedTime() * 0.1;
+    mesh.current.rotation.x = clock.getElapsedTime() * 0.1;
+  });
+
+  return (
+    <>
+      <primitive ref={mesh} object={gltf.scene} />;
+      <OrbitControls enableZoom={false} />
+    </>
+  );
 }
 
 export default function Home() {
@@ -20,16 +33,18 @@ export default function Home() {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%) ",
+          background: "#121212",
         }}
       >
         <ambientLight intensity={0} />
         <pointLight position={[10000, 2000, 1200]} />
+
         <Suspense fallback={null}>
           <Model />
-          <Environment preset="studio" />
+          <Environment preset="sunset" />
         </Suspense>
-        <OrbitControls />
       </Canvas>
+      <Hero />
     </main>
   );
 }
